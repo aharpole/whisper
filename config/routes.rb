73185fcd6498719806f8af378f5fcac5e3c74f00
Aaron Harpole <aharpole@gmail.com>
2015-05-04 18:54:13 -0700

    adding health monitor

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   resources :rooms do
     resources :topics, only: :new
   end
-  
+  get '/healthmonitor' => 'health#ping'
   get '/:slug' => 'rooms#show_by_slug'
   
   resources :topics, only: [:create, :show]
   resources :responses, only: :create
+  
+  
 end
